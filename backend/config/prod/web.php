6e1bdcc23f1913f7df2c3034efd836cdd4791d4b
actors315 <actors315@gmail.com>
2020-11-23 20:26:38 +0800

    配置文件

--- backend/config/prod/web.php
+++ backend/config/prod/web.php
@@ -3,25 +3,22 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=db_order_flow',
-            'username' => getenv('OMS_AUTH_DB_USER'),
-            'password' => getenv('OMS_AUTH_DB_PASS'),
-            // Schema cache options (for production environment)
-            'enableSchemaCache' => true,
-            'schemaCacheDuration' => 60,
-            'schemaCache' => 'cache',
-        ],
-        'authDb' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=db_twinkle_ucenter',
-            'username' => getenv('OMS_AUTH_DB_USER'),
-            'password' => getenv('OMS_AUTH_DB_PASS'),
-            // Schema cache options (for production environment)
+            'dsn' => 'mysql:host=mysql;dbname=db_oms',
+            'username' => getenv('OMS_DB_USER'),
+            'password' => getenv('OMS_DB_PASS'),
             'enableSchemaCache' => true,
             'schemaCacheDuration' => 60,
             'schemaCache' => 'cache',
         ],
         'cache' => [
             'keyPrefix' => 'oms:',
+            'redis' => [
+                'parameters' => [
+                    'host' => 'redis-master',
+                    'port' => 6379,
+                ],
+
+            ]
         ],
     ],
     'modules' => [
--- backend/htdocs/index.php
+++ backend/htdocs/index.php
@@ -3,8 +3,8 @@
 // comment out the following two lines when deployed to production
 use backend\base\Application;
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', getenv('ENV'));
+defined('YII_DEBUG') or define('YII_DEBUG', 'dev' === YII_ENV);
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
