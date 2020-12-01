5a20fa6f788b0866d9de7195079529e0cabe886f
actors315 <actors315@gmail.com>
2020-12-01 20:14:49 +0800

    配置从环境变量注入

--- backend/config/prod/web.php
+++ backend/config/prod/web.php
@@ -1,20 +1,29 @@
 <?php
 
+$env = [
+    'host' => getenv('OMS_DB_HOST') ? getenv('OMS_DB_HOST') : 'mysql',
+    'db' => getenv('OMS_DB_NAME') ? getenv('OMS_DB_NAME') : 'db_oms',
+    'user' => getenv('OMS_DB_USER') ? getenv('OMS_DB_USER') : 'root',
+    'pass' => getenv('OMS_DB_PASS') ? getenv('OMS_DB_PASS') : '',
+    'redis_host' => getenv('OMS_REDIS_HOST') ? getenv('OMS_REDIS_HOST') : 'redis-master',
+    'redis_prefix' => getenv('OMS_REDIS_KEY_PREFIX') ? getenv('OMS_REDIS_KEY_PREFIX') : 'oms:',
+];
+
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=mysql;dbname=db_oms',
-            'username' => getenv('OMS_DB_USER'),
-            'password' => getenv('OMS_DB_PASS'),
+            'dsn' => "mysql:host={$env['host']};dbname={$env['db']}",
+            'username' => $env['user'],
+            'password' => $env['pass'],
             'enableSchemaCache' => true,
             'schemaCacheDuration' => 60,
             'schemaCache' => 'cache',
         ],
         'cache' => [
-            'keyPrefix' => 'oms:',
+            'keyPrefix' => $env['redis_prefix'],
             'redis' => [
                 'parameters' => [
-                    'host' => 'redis-master',
+                    'host' => $env['redis_host'],
                     'port' => 6379,
                 ],
 
